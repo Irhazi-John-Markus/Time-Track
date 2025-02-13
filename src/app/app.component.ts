8f259cd9c6deda2beb090a2c9729e4ba275a6990
Irhazi-John-Markus <john.irhazi@emeal.nttdata.com>
2025-02-13 16:04:35 +0200

    test-done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatSlideToggle } from '@angular/material/slide-toggle';
+
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
-    MatSlideToggle,
-
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
