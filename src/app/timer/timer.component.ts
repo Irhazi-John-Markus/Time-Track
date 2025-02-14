da1ff1f8e5f9a448045e425af2836c2e9ae8872e
Irhazi-John-Markus <john.irhazi@emeal.nttdata.com>
2025-02-14 14:15:24 +0200

    updated-timer

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -1,33 +1,23 @@
+// filepath: /c:/Users/joirhazi/OneDrive - NTT DATA EMEAL/Desktop/Time-Track/src/app/timer/timer.component.ts
 import { Component } from '@angular/core';
-import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-timer',
   templateUrl: './timer.component.html',
-  // Removed direct use of number pipe in the template
-
-  styleUrls: ['./timer.component.css'],
-  providers: [DecimalPipe]
+  styleUrls: ['./timer.component.css']
 })
 export class TimerComponent {
   startTime: number | null = null;
   elapsedTime: number = 0;
   timerInterval: any;
-
-  constructor(private decimalPipe: DecimalPipe) {}
-
-  getFormattedTime(): string {
-    return this.decimalPipe.transform(this.elapsedTime / 1000, '1.0-0') + ' seconds';
-  }
-
-  // Added method to use in the template
-
+  progress: number = 0;
 
   startTimer() {
     if (!this.startTime) {
       this.startTime = Date.now() - this.elapsedTime;
       this.timerInterval = setInterval(() => {
         this.elapsedTime = Date.now() - this.startTime!;
+        this.progress = (this.elapsedTime / (2 * 60 * 60 * 1000)) * 100; // Progress for 2 hours
       }, 1000);
     }
   }
@@ -37,5 +27,9 @@ export class TimerComponent {
       clearInterval(this.timerInterval);
       this.startTime = null;
     }
+  }
+
+  getFormattedTime(): string {
+    return (this.elapsedTime / 1000).toFixed(0) + ' seconds';
   }
 }
