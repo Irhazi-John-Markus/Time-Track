afba02293156a05846927bf824da83acc86fdb3d
Irhazi-John-Markus <john.irhazi@emeal.nttdata.com>
2025-02-19 15:49:26 +0200

    added-big-changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,39 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
 import { AppComponent } from './app.component';
 import { TimerComponent } from './timer/timer.component';
 import { HourglassComponent } from './hourglass/hourglass.component';
 import { ProgressBarComponent } from './progress-bar/progress-bar.component';
+import { SummaryComponent } from './summary/summary.component';
+import { CurrentSituationComponent } from './current-situation/current-situation.component';
+import { SearchFilterComponent } from './search-filter/search-filter.component';
 
 @NgModule({
-  declarations: [  ],
+  declarations: [],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    MatTabsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule,
+    AppComponent,
+    TimerComponent,
+    HourglassComponent,
+    ProgressBarComponent,
+    SummaryComponent,
+    CurrentSituationComponent,
+    SearchFilterComponent
   ],
   providers: [],
 })
---
+++ src/app/current-situation/current-situation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-current-situation',
+  imports: [],
+  templateUrl: './current-situation.component.html',
+  styleUrl: './current-situation.component.css'
+})
+export class CurrentSituationComponent {
+
+}
